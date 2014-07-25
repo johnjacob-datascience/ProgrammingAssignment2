d51ee5c7608f88c9da11199a6d3bdfb0e341f0b7
johnjacob-datascience <robert.christopher.game+coursera@gmail.com>
2014-07-25 23:48:06 +0200

    First commit with a few comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,7 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+## Submission for Coursera R Programming Assignment 2 by John Jacobjingleheimerschmidt
+
 
 ## Write a short comment describing this function
 
